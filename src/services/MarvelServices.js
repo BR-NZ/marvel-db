623c1a127c6c544d8ec8e4bf46b84d33259f8532
Alexey Beschetnikoff <bronzspawn@gmail.com>
2023-04-15 10:35:08 +0700

    Добавлены появление спиннера, ошибки в компоненте RandomChar, добавлены события жизненного цикла.

---
+++ src/services/MarvelServices.js
@@ -0,0 +1,32 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com/';
+    _apiKey = '01f15876ae27789f7db0371fd64cb559';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+        if(!res.ok) throw new Error(`Coulnd't fetch ${url}, status: ${res.status}`);
+        return await res.json();
+    }
+
+    getAllCharacteries = async () => {
+        const res = await this.getResource(`${this._apiBase}v1/public/characters?limit=9&offset=210&apikey=${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+       const res = await this.getResource(`${this._apiBase}v1/public/characters/${id}?apikey=${this._apiKey}`);
+       return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (person) => {
+        return {
+            name: person.name,
+            description: person.description,
+            thumbnail: person.thumbnail.path + '.' + person.thumbnail.extension,
+            homepage: person.homepage,
+            wiki: person.wiki
+        }
+    }
+}
+
+export default MarvelService;
